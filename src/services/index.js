ed3898ec2cd1902e58dd92e79f6a51960c549b1c
belinDEV <orlando2eb@gmail.com>
2024-06-13 08:36:30 -0300

    criando imports

--- src/services/index.js
+++ src/services/index.js
@@ -6,8 +6,9 @@ import cladoService from "./clado";
 import familiaService from "./familia";
 import subFamiliaService from "./subFamilia";
 import metodoLocomocao from "./metodoLocomocao";
-import user from "./user";
-
+import filo from "./filo";
+import genero from "./genero";
+import user from "./user"
 const API_URL = "http://localhost:8070";
 
 const httpClient = axios.create({
@@ -21,8 +22,8 @@ export default {
   clado: cladoService(httpClient),
   familia: familiaService(httpClient),
   subFamilia: subFamiliaService(httpClient),
-  filo: filo(httpClient),
-  genero: genero(httpClient),
-  metodoLocomocao: metodoLocomocao(httpClient),
-  user: user(httpClient)
+  filo: filoService(httpClient),
+  genero: generoService(httpClient),
+  metodoLocomocao: metodoLocomocaoService(httpClient),
+  user: userService(httpClient)
 }
