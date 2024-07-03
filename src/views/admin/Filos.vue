52f12fb31be11b5fbe2a1fd29dccb81ec18b22cc
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-07-03 12:17:33 -0300

    Ajuste link e tag

--- src/views/admin/Filos.vue
+++ src/views/admin/Filos.vue
@@ -1,37 +1,39 @@
 <template>
-    <main>
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-6">
-                        <h5 class="card-title">Filos</h5>
-                    </div>
-                    <div class="col-md-6 text-end">
-                        <router-link to="/admin/cadastrar/filo" class="btn-new" title="New">Novo Filo</router-link>
-                    </div>
-                </div>
-            </div>
-            <img src="@/assets/imagens/painel/img3.jpg" class="card-img-bottom" alt="Imagem de capa do card">
+  <main>
+    <div class="card">
+      <div class="card-body">
+        <div class="row">
+          <div class="col-md-6">
+            <h5 class="card-title">Filos</h5>
+          </div>
+          <div class="col-md-6 text-end">
+            <router-link to="/admin/cadastrar/filo" class="btn-new" title="New">Novo Filo</router-link>
+          </div>
         </div>
-        <table class="table table-dark table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Tipo</th>
-                    <th scope="col">Opções</th>
-                </tr>
-            </thead>
-            <tbody>
-              <tr v-for="filo in state.filos" :key="filo.id">
-                <td scope="row">{{ filo.id }}</td>
-                <td>{{ filo.tipo }}</td>
-                <td><a href="" class="btn btn-primary" title="Alterar"> <i class="bi bi-pencil"></i></a>
-                  <a @click="deleteFilo(filo.id)" class="btn btn-danger" title="Deletar"><i class="bi bi-trash"></i></a>
-                </td>
-              </tr>
-            </tbody>
-        </table>
-    </main>
+      </div>
+      <img src="@/assets/imagens/painel/img3.jpg" class="card-img-bottom" alt="Imagem de capa do card">
+    </div>
+    <table class="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Tipo</th>
+          <th scope="col">Opções</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="filo in state.filos" :key="filo.id">
+          <td scope="row">{{ filo.id }}</td>
+          <td>{{ filo.tipo }}</td>
+          <td>
+            <router-link :to="{ name: 'alterar-filo',  params: { id: filo.id },}" class="btn btn-primary" title="Alterar"><i class="bi bi-pencil"></i>
+            </router-link>
+            <a @click="deleteFilo(filo.id)" class="btn btn-danger" title="Deletar"><i class="bi bi-trash"></i></a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
 </template>
 
 <script setup>
@@ -67,58 +69,58 @@ onMounted(getFilos);
 
 <style scoped>
 a {
-    text-decoration: none;
+  text-decoration: none;
 }
 
 main {
-    margin: 100px 10px 10px 10px;
+  margin: 100px 10px 10px 10px;
 }
 
 .card {
-    background: #212121;
-    border-radius: 10px 10px 0px 0px;
+  background: #212121;
+  border-radius: 10px 10px 0px 0px;
 }
 
 .card h5 {
-    color: #0f5;
+  color: #0f5;
 }
 
 .card-img-bottom {
-    border-radius: 0;
+  border-radius: 0;
 }
 
 .card img {
-    width: 100%;
-    height: 100px;
-    object-fit: cover;
-    object-position: center;
+  width: 100%;
+  height: 100px;
+  object-fit: cover;
+  object-position: center;
 }
 
 .btn-new {
-    background: transparent;
-    color: #0f5;
-    padding: 10px 20px;
-    border-radius: 4px;
-    transition: 0.3s;
-    border: transparent 1px solid;
+  background: transparent;
+  color: #0f5;
+  padding: 10px 20px;
+  border-radius: 4px;
+  transition: 0.3s;
+  border: transparent 1px solid;
 }
 
 .btn-new:hover {
-    border: #0f5 1px solid;
+  border: #0f5 1px solid;
 
 }
 
 th {
-    color: #0f5;
+  color: #0f5;
 }
 
 th,
 td {
-    background: #212121;
-    text-align: center;
+  background: #212121;
+  text-align: center;
 }
 
 .btn {
-    margin-right: 5px;
+  margin-right: 5px;
 }
 </style>
