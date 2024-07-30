98bd36e00e7c0ef56fb9cf20b174d3c88dd77e09
belinDEV <orlando2eb@gmail.com>
2024-07-30 14:05:13 -0300

    criando components

---
+++ src/components/Carrocel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active" data-bs-interval="5000">
+        <img id="image_banner" src="./../assets/imagens/banners/banner_1.jpg" class="d-block w-100"
+          alt="Banner Dino Um">
+      </div>
+      <div class="carousel-item" data-bs-interval="5000">
+        <img id="image_banner" src="./../assets/imagens/banners/banner_2.jpg" class="d-block w-100"
+          alt="Banner Dino Dois">
+      </div>
+    </div>
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+#image_banner {
+  height: 100vh;
+  object-fit: cover;
+}
+
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,7 @@
 <template>
   <main>
     <section id="section_1" style="position: relative">
-      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="5000">
-            <img id="image_banner" src="./../assets/imagens/banners/banner_1.jpg" class="d-block w-100"
-              alt="Banner Dino Um">
-          </div>
-          <div class="carousel-item" data-bs-interval="5000">
-            <img id="image_banner" src="./../assets/imagens/banners/banner_2.jpg" class="d-block w-100"
-              alt="Banner Dino Dois">
-          </div>
-        </div>
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
-            aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-            aria-label="Slide 2"></button>
-        </div>
-      </div>
+      <Carrocel/>
       <div class="div_linear_gradiant">
         <Header />
         <div class="container">
@@ -38,6 +21,7 @@
 <script setup>
 import Header from '@/components/Header.vue';
 import Button from '@/components/ButtonExplore.vue';
+import Carrocel from'@/components/Carrocel.vue';
 </script>
 
 <style scoped>
