ea10b915de21ae58f5499aea08fa7c9d4f2a2d72
belinDEV <orlando2eb@gmail.com>
2024-08-02 08:55:14 -0300

    criando a section curiosidades

--- src/components/Carrocel.vue
+++ src/components/Carrocel.vue
@@ -2,7 +2,7 @@
   <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active" data-bs-interval="5000">
-        <img id="image_banner" src="./../assets/imagens/banners/banner_1.jpg" class="d-block w-100"
+        <img id="image_banner" src="./../assets/imagens/banners/banner_2.jpg" class="d-block w-100"
           alt="Banner Dino Um">
       </div>
       <div class="carousel-item" data-bs-interval="5000">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <section id="section_1" style="position: relative">
-      <Carrocel/>
+      <Carrocel />
       <div class="div_linear_gradiant">
         <Header />
         <div class="container">
@@ -15,16 +15,85 @@
         </div>
       </div>
     </section>
+    <section class="services spad" style="position: relative">
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="services__title">
+              <div class="section-title">
+                <span>Curiosidades sobre os dinossauros!!</span>
+                <h2>O que é o Dino Gallery?</h2>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.n, ante urna consectetur urna, sed tempus sem
+                nulla a nulla.
+                Phasellus in aliquam lorem. Etiam efficitur urn</p>
+              <a href="#" class="primary-btn">bla bla bla</a>
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="row">
+              <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="coluna_infoDino">
+                  <div class="coluna_infoDino__icon">
+                    <i class=" "></i>
+                  </div>
+                  <h4>Maior Predador:</h4>
+                  <p>O Tyrannosaurus rex é um dos predadores mais conhecidos, com uma mordida poderosa capaz de esmagar ossos. Ele viveu no final do período Cretáceo, há cerca de 68 a 66 milhões de anos.</p>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="coluna_infoDino">
+                  <div class="coluna_infoDino__icon">
+
+                  </div>
+                  <h4>Fósseis pelo Mundo:</h4>
+                  <p>Fósseis de dinossauros foram encontrados em todos os continentes, incluindo a Antártida, demonstrando que esses animais eram extremamente adaptáveis e diversificados.</p>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="coluna_infoDino">
+                  <div class="coluna_infoDino__icon">
+
+                  </div>
+                  <h4>Maior Dinossauro Terrestre: </h4>
+                  <p>O Patagotitan mayorum é considerado um dos maiores dinossauros terrestres, medindo cerca de 37 metros de comprimento e pesando em torno de 69 toneladas.</p>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6">
+                <div class="coluna_infoDino">
+                  <div class="coluna_infoDino__icon">
+
+                  </div>
+                  <h4>Herbívoros Gigantes: </h4>
+                  <p>Alguns dos maiores dinossauros, como os saurópodes (ex: Brachiosaurus e Diplodocus), eram herbívoros que se alimentavam de grandes quantidades de vegetação.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </section>
     <h1>Home</h1>
   </main>
 </template>
 <script setup>
 import Header from '@/components/Header.vue';
 import Button from '@/components/ButtonExplore.vue';
-import Carrocel from'@/components/Carrocel.vue';
+import Carrocel from '@/components/Carrocel.vue';
 </script>
 
 <style scoped>
+@media (min-width: 992px) {
+  .col-lg-4 {
+    -ms-flex: 0 0 33.333333%;
+    flex: 0 0 33.333333%;
+    max-width: 33.333333%;
+  }
+}
+
+
 #image_banner {
   height: 100vh;
   object-fit: cover;
@@ -46,6 +115,42 @@ import Carrocel from'@/components/Carrocel.vue';
   height: 100vh;
   object-fit: cover;
 }
+
+.coluna_infoDino {
+  color: #0f5;
+ 
+ }
+
+.coluna_infoDino p {
+  font-size: 16px;
+  font-family: "Josefin Sans", sans-serif;
+  color: #fff;
+  font-weight: 400;
+  line-height: 26px;
+  margin-bottom: 30px;
+  position: relative;
+  width: 100%;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: left;
+  
+}
+
+.coluna_infoDino h4 {
+  font-family: 'Poppins';
+}
+
+.services {
+  background: #111;
+  color: #fff;
+  height: 100vh;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
 
 .content_1 {
   display: flex;
